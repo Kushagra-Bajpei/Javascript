4a0cdde4e0a0cf1f547bc5f0c2058438c33e608b
Kushagra Bajpei <kushagrabajpeimbic@gmail.com>
2025-09-20 15:44:55 +0530

    send

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));  
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+// "abc" => NaN
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+
+// 0 => false; 1 => true
+// "" => false; 
+// "abc" => true
+// null => false
+// undefined => false
+// " " => true
+// [] => true
+// {} => true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
