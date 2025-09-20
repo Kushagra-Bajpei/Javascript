340d674313000e6970f66ed85021295ed355d858
Kushagra Bajpei <kushagrabajpeimbic@gmail.com>
2025-09-20 22:32:03 +0530

    send

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types : string, number, bigint, boolean, undefined, symbol, null
+
+const score = 100; // number
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId); 
+
+// const bigNumber = 1234567890123456789012345678901234567890n;
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"];
+let myObj = {
+  name: "Ravi",
+  age: 22,
+};
+
+const myFunction = function () {
+  console.log("Hello World");
+}
+
+console.log(typeof anotherId);
