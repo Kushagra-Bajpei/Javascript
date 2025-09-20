5f73245a6f6762a0a9210543cffca918838b3517
Kushagra Bajpei <kushagrabajpeimbic@gmail.com>
2025-09-20 20:08:42 +0530

    update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,3 +37,47 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+// **********************************  Operations  **********************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue); 
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "Hello ";
+let str2 = "Kushagra";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);  // "12"
+// console.log(1 + "2");  // "12"
+// console.log("1" + 2 + 2);  // "122"
+// console.log(1 + 2 + "2");  // "32"  => line 63 and 64 is not correct way
+
+// console.log( (3 + 2) * 2 % 3 );
+
+// console.log(+true);  // 1
+// console.log(true+);  // error
+// console.log(+"");    // 0
+// console.log(+null);  // 0
+// console.log(+undefined); // NaN
+// console.log(+ "33abc");  // NaN
+// console.log(+ "33");     // 33
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++; // ++gameCounter => both always return same value
+console.log(gameCounter);  
+
