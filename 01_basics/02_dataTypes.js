2a796aa00a7d377da5f31c4cc399281c6e2c101d
Kushagra Bajpei <kushagrabajpeimbic@gmail.com>
2025-09-20 15:09:59 +0530

    send

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";    // treat all JS code as newer version
+
+// alert("3 + 3")  // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3);          // code readability should be high
+    
+console.log("Kushagra");
+
+
+let name = "Kushagra";
+let age = 21;
+let isLoggedIn = false;
+
+
+// number => 2 to the power of 53
+// bigint => larger data type to store big numbers specially for traing, stock market
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);
