29fb19179ac72950bad692f12bda3f0ef6a1eefc
Kushagra Bajpei <kushagrabajpeimbic@gmail.com>
2026-03-03 23:19:27 +0530

    send

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -62,8 +62,8 @@ let negValue = -value;
 // console.log(2 / 3);
 // console.log(2 % 3);
 
-let str1 = "Hello ";
-let str2 = "Kushagra";
+let str1 = "Hello";
+let str2 = " Kushagra";
 
 let str3 = str1 + str2;
 // console.log(str3);
